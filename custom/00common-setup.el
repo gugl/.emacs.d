260cc39f92d1545d9445ce4fbcfa7c688c0ad673
gugl <guenter.glueck@gmail.com>
2015-03-30 16:51:36 +0200

    added Cask and pallet, added first common-setup

---
+++ custom/00common-setup.el
@@ -0,0 +1,5 @@
+;; Navigate between windows using Alt-1, Alt-2, Shift-left, shift-up, shift-right
+(windmove-default-keybindings)
+
+;; Enable copy and pasting from clipboard
+(setq x-select-enable-clipboard t)
---
+++ init.el
@@ -0,0 +1,8 @@
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+(require 'pallet)
+(pallet-mode t)
+(add-to-list 'load-path "~/.emacs.d/custom")
+
+(load "00common-setup.el")
+
