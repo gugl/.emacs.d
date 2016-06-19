cadf9fccafdc3ba3e5eb0c9c0f120239021da8cb
gugl <guenter.glueck@gmail.com>
2016-06-19 18:07:14 +0200

    Disable netree smart open
    It was annoying that it opened the tree with
    the current buffers directory.

--- custom/00common-setup.el
+++ custom/00common-setup.el
@@ -69,7 +69,8 @@
 (global-set-key [f8] 'neotree-toggle)
 
 ;; Every time when the neotree window is opened, let it find current file and jump to node.
-(setq neo-smart-open t)
+;; (setq neo-smart-open t)
+(setq neo-smart-open nil)
 
 ;; When running ‘projectile-switch-project’ (C-c p p), ‘neotree’ will change root automatically.
 (setq projectile-switch-project-action 'neotree-projectile-action)
