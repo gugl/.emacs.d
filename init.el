ad43697b17d7b892c3d189f33d98a5251fe16c5c
gugl <guenter.glueck@gmail.com>
2017-02-09 04:05:43 +0100

    Added ivy, swiper, counsel, and a lot of other useful packages

--- init.el
+++ init.el
@@ -12,8 +12,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bb749a38c5cb7d13b60fa7fc40db7eced3d00aa93654d150b9627cabd2d9b361" default)))
  '(display-battery-mode nil)
  '(global-rbenv-mode t)
+ '(package-selected-packages
+   (quote
+    (slack git-timemachine avy counsel-osx-app counsel-projectile counsel ivy-hydra swiper cider use-package guide-key mu4e-maildirs-extension dashboard page-break-lines chess vimish-fold notmuch dumb-jump annotate calfw yaml-mode web-mode twittering-mode tabbar smartparens slim-mode scss-mode sass-mode robe rinari react-snippets rbenv rainbow-mode railscasts-theme projectile-rails powerline pallet org-journal neotree monokai-theme magit-gitflow highlight-symbol highlight-indentation helm grizzl goto-gem flycheck feature-mode expand-region exec-path-from-shell engine-mode discover-my-major discover-js2-refactor diminish diff-hl darcula-theme color-theme-sanityinc-tomorrow coffee-mode bundler auto-complete alchemist ag)))
  '(rbenv-installation-dir "/usr/local/opt/rbenv")
  '(rbenv-show-active-ruby-in-modeline nil)
  '(size-indication-mode nil))
